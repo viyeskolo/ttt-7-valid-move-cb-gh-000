62d3d9c562107536a500601bb602ffb61c1f9a97
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-25 20:00:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,23 +1,25 @@
-def valid_move?(board, position)
-	spot = position.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
-		true
-
-	else
-		false
-
-	end
-
+def current_player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, position)
-  if board[position] == " " || board[position] == "" || board[position] == nil
-  	false
-
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    player_move(board, index, current_player(board))
+    display_board(board)
   else
-  	true
-
+    turn(board)
   end
+end
 
-end 
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  }
+  counter
+end
